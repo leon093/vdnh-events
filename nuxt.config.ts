eecd1664d7397038cff39bc08830a999c455748b
leon <leon093@gmail.com>
2024-02-24 21:30:43 +0300

    svgLoader

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import svgLoader from 'vite-svg-loader'
+
 export default defineNuxtConfig({
     devtools: { enabled: false },
     css: ["~/assets/css/reset.css", "~/assets/css/styles.css"],
@@ -6,5 +8,8 @@ export default defineNuxtConfig({
         head: {
             title: "ВДНХ",
         },
+    },
+    vite: {
+        plugins: [svgLoader()]
     }
 });
